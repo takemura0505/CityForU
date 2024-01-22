f258e0ec40c79259c1470f53c95197ed090338c7
takemuraharuumi <takemuraharuumi@gmail.com>
2024-01-22 23:39:22 +0900

    FortuneVCにscrollView追加、UI更新

--- CityForU/FortuneViewController.swift
+++ CityForU/FortuneViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 class FortuneViewController: UIViewController {
     
     @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setButtonUI(button: nextButton)
+        //キーボードの表示非表示を確認
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        //キーボード通知を解除
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        let keyboardHeight = keyboardSize.height
+        scrollView.contentInset.bottom = keyboardHeight
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        scrollView.contentInset = .zero
     }
     
     private func setButtonUI(button: UIButton) {
